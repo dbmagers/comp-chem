2ffa883bab85a2492a98df06bc3e0ce9a12fe96a
D. Brandon Magers <bmagers@belhaven.edu>
2022-01-24 14:19:22 -0500

    initial update of qinfo to python3

--- torque/qinfo.py
+++ torque/qinfo.py
@@ -1,4 +1,4 @@
-#!/opt/miniconda3/envs/python2.7/bin/python
+#!/usr/bin/python3
 
 ## J. Agarwal
 ## Rev 1: 5/24/13
@@ -6,9 +6,10 @@
 ## Rev 2.1: 7/11/13
 ## Rev 2.2 7/26/13
 ## Rev 2.3 12/27/16 D. Brandon Magers
+## Rev 3 __/__ /20 Rayley Farnam
 
 ### Import required modules ###
-import commands
+import subprocess
 import xml.etree.ElementTree as ET
 from optparse import OptionParser as OP
 import time
@@ -18,8 +19,9 @@ import yaml
 
 ### Functions ###
 
+
 def qstat(path):
-   if os.path.exists(path): return commands.getoutput(path+" -f -x") 
+   if os.path.exists(path): return subprocess.getoutput(path+" -f -x") 
    else: 
       print("qstat command not found...exiting.")
       sys.exit()
@@ -47,6 +49,7 @@ def find_job_data(job):
       jobWalltime = jobWalltime.find('walltime').text
    return [jobUser, jobName, jobState, jobID, jobPPN, jobWalltime]
 
+#functions to color code the output
 def red(phrase): return("\033[91m"+phrase+"\033[0m") 
 def grey(phrase): return("\033[90m"+phrase+"\033[0m")
 def green(phrase): return("\033[92m"+phrase+"\033[0m")
@@ -59,7 +62,7 @@ def get_qinfo_data(options):
    jobInfo = {} # queue: [[user, name, state, id]]
    queueInfo['batch'] = [15,0]
    # Parse qstat output
-   qstatOutput = qstat(commands.getoutput("which qstat"))
+   qstatOutput = qstat(subprocess.getoutput("which qstat"))
    if qstatOutput == "":
       return queueInfo, jobInfo
    qstatXml = ET.fromstring(qstatOutput)
@@ -67,16 +70,16 @@ def get_qinfo_data(options):
    # Data structures
    for job in qstatXml.iter("Job"):
       data = find_job_data(job)
-      if jobInfo.has_key('batch'): jobInfo['batch'].append(data)
+      if jobInfo.__contains__('batch'): jobInfo['batch'].append(data)
       else: jobInfo["batch"] = [data]
    return queueInfo, jobInfo
 
 def print_qinfo_data(queueInfo, jobInfo, options): 
-   allbox = u''.join(unichr(9472 + x) for x in range(200))
+   allbox = u''.join(chr(9472 + x) for x in range(200))
    box = [ allbox[i] for i in (2, 0, 12, 16, 20, 24, 44, 52, 28, 36, 60) ]
    (vbar, hbar, ul, ur, ll, lr, nt, st, wt, et, plus) = box
    # Print title card
-#   print(blue((hbar*26)+"\n# qinfo//ja+dbm//ver2.3  #\n"+(hbar*26)+"\n"))
+
    # Column widths and misc information
    nCol = 5 # Job Number column
    fCol = 12 # First column width
@@ -95,7 +98,7 @@ def print_qinfo_data(queueInfo, jobInfo, options):
    for queue in sorted(queueInfo):
       used = str(queueInfo[queue][1])
       avail = str(queueInfo[queue][0] - queueInfo[queue][1])
-#      headers += (queue.split(".")[0]+" ("+used+" used/"+avail+" avail)").center(colSum+1)
+
       headers += (queue.split(".")[0]+" ("+avail+" avail)").center(colSum+1)
    print(headers+"\n"+dashLine)
    print((label*len(queueInfo))+"\n"+dashLine)
@@ -126,56 +129,34 @@ def print_qinfo_data(queueInfo, jobInfo, options):
       else: print(blank)
    else:
       print(" No current jobs".ljust(colSum)+endLine)
-#   print(dashLine)
-#   print("Recently Completed Jobs:".rjust(26)+"".ljust((colSum+1) * len(queueInfo) - 27)+endLine)
-#   completedLines = completedLines[-options.numComplete:]
-#   for j in completedLines:
-#      print(j)
 
    # Summary of additional jobs or blank line
    printLine = ""
    for queue in sorted(queueInfo):
-      if jobInfo.has_key(queue):
+      if jobInfo.__contains__(queue):
          if len(jobInfo[queue]) > options.numJobs:
             printLine += grey((" +"+str(len(jobInfo[queue])-options.numJobs)+" more jobs").center(colSum))+endLine
          else: printLine += blank
       else: printLine += blank
-#   print(printLine)
    print(dashLine)
    completeFile = "/home/"+logname+"/.queue_complete"
    if os.path.exists(completeFile):
       fullWidth = (colSum+1) * len(queueInfo)
-      recentJobs = yaml.load(commands.getoutput("tail -"+str(6*options.completed)+" "+completeFile))
+      recentJobs = yaml.safe_load(subprocess.getoutput("tail -"+str(6*options.completed)+" "+completeFile))
       if len(recentJobs) != 0: 
          print((" Recently Completed Jobs for "+options.logname+":").ljust(58)+endLine)
          print(bold("ID".center(nCol))+bold("  Job Name".ljust(fCol+sCol))+bold("Stop Time".ljust(tCol+frCol))+bold("Elap. Time".ljust(ffCol))+endLine)
-#   label = bold("ID".center(nCol))+bold("  User".ljust(fCol))+bold("Job Name".ljust(sCol))+bold("Proc.".ljust(tCol))+bold("St.".ljust(frCol))+bold("Elap. Time".ljust(ffCol))+endLine
          for job in recentJobs: 
             stop = ' '.join(recentJobs[job]["Stop"].split()[1:4])
             printLine = str(job).rjust(4)+"  "+recentJobs[job]["Dir"].split("/")[-1].ljust(24)+" "+stop+"  "+recentJobs[job]["Elapsed Time"]
-#            printLine = str(job).rjust(4)+"  "+recentJobs[job]["Dir"].split("/")[-1].ljust(34)+" "+stop
-#            printLine = str(job).rjust(4)+"  "+recentJobs[job]["Dir"].replace("/home/"+os.environ["LOGNAME"],"~").ljust(34)+" "+stop
-#            printLine = t[1].rjust(10)+" on "+t[7].split("/")[1][:-1]+": "+t[9].replace("/home/"+os.environ["LOGNAME"],"~")
             print(printLine.ljust(fullWidth-1)+endLine)
          print(dashLine)
 
-#   completeFile = "/home/"+os.environ["LOGNAME"]+"/.queue_completed.txt"
-#   if os.path.exists(completeFile):
-#      fullWidth = (colSum+1) * len(queueInfo)
-#      recentJobs = commands.getoutput("tail -"+str(options.completed)+" "+completeFile).split("\n")
-#      if len(recentJobs[0]) != 0: 
-#         print("Recently Completed Jobs 2:".rjust(26)+"".ljust(fullWidth-27)+endLine)
-#         for job in recentJobs: 
-#            t = job.split()
-#            printLine = t[1].rjust(10)+" on "+t[7].split("/")[1][:-1]+": "+t[9].replace("/home/"+os.environ["LOGNAME"],"~")
-#            print("".rjust(5)+printLine.ljust(fullWidth-6)+endLine)
-#         print(dashLine)
-
 ### Main ###
-#def main():
+
 
 # Parse arguments
-_version = "2.3"
+_version = "3"
 _description = "qinfo"
 parser = OP(add_help_option=True, version=_version, description=_description)
 parser.add_option("--watch", default=False, action="store_true", dest="watch",
